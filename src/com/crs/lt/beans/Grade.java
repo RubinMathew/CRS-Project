6d5b58e9cb663c22499def2c7242d0040ef71774
rubinmathew <rubinullattu129@gmail.com>
2022-02-11 09:28:15 +0530

    This is the fifth commmit

---
+++ src/com/crs/lt/beans/Grade.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package com.crs.lt.beans;
+
+/**
+ * @author user214
+ *
+ */
+public class Grade {
+	
+	private String grade;
+	
+	public String getGrade() {
+		return grade;
+	}
+
+	public void setGrade(String grade) {
+		this.grade = grade;
+	}
+
+
+	
+
+}
